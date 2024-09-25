a009823f35a85fb657693ec25373c0877ddeac77
BagasArdiansaPutra <bagasardiansa06@gmail.com>
2024-09-25 10:25:08 +0700

    Add files via upload

---
+++ KategoriUsia.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class KategoriUsia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan usia Anda: ");
+        int usia = sc.nextInt();
+        
+        if (usia < 0) {
+            System.out.println("Input tidak valid. Harap masukkan angka positif.");
+            return;
+        }
+        
+        String kategori;
+        if (usia >= 0 && usia <= 12) {
+            kategori = "Anak";
+        } else if (usia >= 13 && usia <= 19) {
+            kategori = "Remaja";
+        } else if (usia >= 20 && usia <= 64) {
+            kategori = "Dewasa";
+        } else {
+            kategori = "Lansia";
+        }
+        System.out.println("Kategori usia Anda: " + kategori);
+    }
+}
